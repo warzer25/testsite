29ef7cf284a89d0fafc2532ba6ac45a14f901f0c
warzer25 <98097043+warzer25@users.noreply.github.com>
2025-01-05 05:52:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -109,9 +109,7 @@ function updateClock(date) {
 function displayGroupMessage(nextSundayWeekNumber) {
     const message = document.getElementById("groupMessage");
     if (nextSundayWeekNumber % 2 !== 0) {
-        message.textContent = "Group B is first this week!";
     } else {
-        message.textContent = "Group A is first this week!";
     }
 }
 
